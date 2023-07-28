35923d59731c1b04e8cc9322874f2cd0c959368b
rahmanfardin <mfardinr@gmail.com>
2023-07-28 22:37:01 +0600

    wrong

--- header/cc.hpp
+++ header/cc.hpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <stack>
+#include <vector>
 using namespace std;
 
 class cc
@@ -7,11 +8,14 @@ class cc
 private:
     int value;        // value of the the money we have to exchange
     stack<int> money; // the set of money
+    vector<int> dp;   // a dp vector
+
 public:
-    cc(int value, stack<int> money)
+    cc(int value, stack<int> money, vector<int> dp)
     {
         this->value = value;
         this->money = money;
+        this->dp = dp;
     }
     ~cc();
 
@@ -34,31 +38,32 @@ public:
         return money;
     }
 
-    void nextMoneyValue()
-    {
-        if (this->value - this->money.top() < 0)
-        {
-            money.pop();
-        }
-    }
-
     int getThenumberOfcoins(int value)
     {
-        //cout << money.top() <<endl;
+        //! create a new money so that we can save the original value
+        stack<int> money1;
+        money1 = money;
+        // cout << money.top() <<endl;
         if (value == 0)
             return 0;
-        if (money.empty())
+        if (money1.empty())
             return -1;
-        
-        if (value - money.top() >= 0)
+        if (dp.at(value) != -1)
         {
-            int result = value / money.top();
-            value -= (result * money.top());
+            cout << "s";
+            return dp.at(value);
+        }
+        if (value - money1.top() >= 0)
+        {
+            int result = value / money1.top();
+            dp.at(value) = result;
+            value -= (result * money1.top());
+            cout << dp.at(value) << endl;
             return result + this->getThenumberOfcoins(value);
         }
         else
         {
-            money.pop();
+            money1.pop();
             return this->getThenumberOfcoins(value);
         }
     }
--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,31 @@
 #include "../header/cc.hpp"
 #include <iostream>
 #include <stack>
+#include <vector>
 using namespace std;
-
 
 int main()
 {
     stack<int> money;
-    cout << "enter the money(enter -1 to finish entering): " << endl; 
-    int value = 0;
-    while (value!= -1)
+    int value = 0, x = 0;
+    vector<int> dp(100, -1);
+    cc *coin;
+
+    cout << "enter the money(enter -1 to finish entering): " << endl;
+
+    while (value != -1)
     {
         cin >> value;
         money.push(value);
     }
     money.pop();
-    cout << "enter the ammount: " << endl;
-    int x;
-    cin >> x;
-    cc *coin = new cc(x, money);
-    int result = coin->getThenumberOfcoins(coin->getValue());
-    cout << result << endl;
+    coin = new cc(x, money, dp);
+    for (int i = 0; i < 2; i++)
+    {
+        cout << "enter the ammount: " << endl;
+        cin >> x;
+        coin->setValue(x);
+        int result = coin->getThenumberOfcoins(coin->getValue());
+        cout << result << endl;
+    }
 }
